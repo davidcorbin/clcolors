a56630ab3a854a45a09fb7e545b43fb734afb694
daconex <daconex@gmail.com>
2014-08-16 01:41:41 -0400

    More colors

--- color.c
+++ color.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdbool.h>
 #include <string.h>
 
 #define RED     "\x1b[31m"
@@ -10,16 +9,18 @@
 #define CYAN    "\x1b[36m"
 #define NONE    "\x1b[0m"
 
-bool verbose = false;
-char *newcolor;
+char *newcolor = "";
 
 int main (int argc, char **argv)
 {
-    if (strcmp(argv[1], "red") == 0) {
-        newcolor = RED;
-    }
-    if (strcmp(argv[1], "white") == 0) {
-        newcolor = NONE;
+    if (strcmp(argv[1], "red") == 0) newcolor = RED;
+    else if (strcmp(argv[1], "white") == 0) newcolor = NONE;
+    else if (strcmp(argv[1], "green") == 0) newcolor = GREEN;
+    else if (strcmp(argv[1], "yellow") == 0) newcolor = YELLOW;
+    else if (strcmp(argv[1], "blue") == 0) newcolor = BLUE;
+    else if (strcmp(argv[1], "magenta") == 0) newcolor = MAGENTA;
+    else {
+        printf("Change console text color by typing: color red.");
     }
 
     printf("%s", newcolor);
