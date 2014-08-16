03052fb3e18d2f1d7cd9d0964acfb0967ce29948
daconex <daconex@gmail.com>
2014-08-16 09:30:55 -0400

    Fixed seg fault

--- color.c
+++ color.c
@@ -13,15 +13,17 @@ char *newcolor = "";
 
 int main (int argc, char **argv)
 {
+    if (!argv[1]) {
+        printf("Color needed to be changed. e.g. color red\n");
+        return 1;
+    }
+
     if (strcmp(argv[1], "red") == 0) newcolor = RED;
     else if (strcmp(argv[1], "white") == 0) newcolor = NONE;
     else if (strcmp(argv[1], "green") == 0) newcolor = GREEN;
     else if (strcmp(argv[1], "yellow") == 0) newcolor = YELLOW;
     else if (strcmp(argv[1], "blue") == 0) newcolor = BLUE;
-    else if (strcmp(argv[1], "magenta") == 0) newcolor = MAGENTA;
-    else {
-        printf("Change console text color by typing: color red.");
-    }
+    else if (strcmp(argv[1], "magenta") == 0) newcolor = MAGENTA; 
 
     printf("%s", newcolor);
     return 0;
